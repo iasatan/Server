f4eb71c5832df0c6beb1fc8e6eccb81aa208b164
Sátán Ádám <satanadam@outlook.com>
2017-09-09 11:24:44 +0200

    Update and rename mp3.sh to removeArtists.sh

--- scripts/torrent/mp3/removeArtists.sh
+++ scripts/torrent/mp3/removeArtists.sh
@@ -20,12 +20,4 @@ function iterate {
 		fi
 	done
 }
-find . -type f -iname \*.jpg -delete -print
-find . -type f -iname \*.png -delete -print
-find . -type f -iname \*.bmp -delete -print
-find . -type f -iname \*.txt -delete -print
-find . -type f -iname \*mix* -delete -print
-find . -type f -iname \*edit* -delete -print
-find . -type f -iname \*remix* -delete -print
-find . -type f -iname \*disney* -delete -print
 iterate
