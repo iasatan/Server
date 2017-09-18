ebf37af073edc0b4c8a1ff796fc2e102c60055b6
Sátán Ádám <satanadam@outlook.com>
2017-09-18 20:29:21 +0200

    Update removeDuplicates.sh

--- scripts/torrent/mp3/removeDuplicates.sh
+++ scripts/torrent/mp3/removeDuplicates.sh
@@ -18,7 +18,7 @@ function delete {
 function check {
 	artist=$(mp3infov2 -p %a "$1")
 	title=$(mp3infov2 -p %t "$1")
-	touch /srv/dev-disk-by-id-ata-WDC_WD10EFRX-68FYTN0_WD-WCC4J4APRZR8-part1/red/music/Server/scripts/torrent/mp3/music/"$artist".txt
+	echo "$artist $title"
 	return $(grep -c "$title" /srv/dev-disk-by-id-ata-WDC_WD10EFRX-68FYTN0_WD-WCC4J4APRZR8-part1/red/music/Server/scripts/torrent/mp3/music/"$artist".txt)
 }
 
