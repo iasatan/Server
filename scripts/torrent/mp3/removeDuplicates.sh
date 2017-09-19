fa509aefdccd27be59d3ec448667301931b89368
iasatan <satanadam@outlook.com>
2017-09-19 06:31:57 +0000

    /srv/dev-disk-by-id-ata-WDC_WD10EFRX-68FYTN0_WD-WCC4J4APRZR8-part1/red/music/[RG.REBOOT] Flight of the Conchords - Album Discography (MP3-320kbps) added

--- scripts/torrent/mp3/removeDuplicates.sh
+++ scripts/torrent/mp3/removeDuplicates.sh
@@ -2,23 +2,22 @@
 
 function add {
 	#sleep 1
-	echo "$artist $title added"
+	artist=$(mp3infov2 -p %a "$1")
+	title=$(mp3infov2 -p %t "$1")
 	echo "$title"  >> /srv/dev-disk-by-id-ata-WDC_WD10EFRX-68FYTN0_WD-WCC4J4APRZR8-part1/red/music/Server/scripts/torrent/mp3/music/"$artist".txt
 }
 
 function delete {
-	function delete {
+	artist=$(mp3infov2 -p %a "$@")
 	if [[ -z "$artist" ]]; then
 		echo "$@ duplicate deleted"
 		rm -f "$@"
 	fi
 }
-}
 
 function check {
 	artist=$(mp3infov2 -p %a "$1")
 	title=$(mp3infov2 -p %t "$1")
-	echo "$artist $title"
 	return $(grep -c "$title" /srv/dev-disk-by-id-ata-WDC_WD10EFRX-68FYTN0_WD-WCC4J4APRZR8-part1/red/music/Server/scripts/torrent/mp3/music/"$artist".txt)
 }
 
@@ -30,11 +29,11 @@ function iterate {
 				iterate
 				cd ..
 			elif [[ -f "$f" ]]; then
-			if [[ check "$f" -gt 0]]; then
-				delete "$f"
-			else 
-				add "$f"
-				#check "$f" && delete "$f" || add 
+				if [[ $(check "$f") -gt 0 ]]; then
+					delete "$f"
+				else 
+					add "$f"
+				fi
 			fi
 		done
 }
