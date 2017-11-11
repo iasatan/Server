820583784f620185954bb20c80298534582ac5e9
Satan Adam <satanadam@outlook.com>
2017-11-11 09:42:26 +0100

    2015 good music stored

--- mp3/bGoodMusic.sh
+++ mp3/bGoodMusic.sh
@@ -4,6 +4,7 @@ function add {
 	artist=$(mp3infov2 -p %a "$1")
 	title=$(mp3infov2 -p %t "$1")
 	echo "$title"  >>/media/iasatan/846543cd-d9c6-4eb5-8c8c-aeee3951834e/linuxScripts/mp3/goodMusic/"$artist".txt
+    echo "$title"
 }
 
 function iterate {
@@ -18,6 +19,7 @@ function iterate {
 			fi
 		done
 }
+find . -type d -empty -delete -print
 iterate
 directory=${PWD##*/}
  echo "$directory" >> /media/iasatan/846543cd-d9c6-4eb5-8c8c-aeee3951834e/linuxScripts/mp3/storedGood.txt
